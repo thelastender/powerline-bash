1a4c5b4adaa912925564bf69769d4502bbdec26c
Ender <thelastender@gmail.com>
2015-02-07 19:07:57 +0800

    基本是一版可以使用git的了，但是有死循环的bug

--- powerline.sh
+++ powerline.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-#lock, network, separator, separator_thin
-symbols=([0]="\xEE\x82\xA2" [1]="\xEE\x82\xA2" [2]="\xEE\x82\xB0" [3]="\xEE\x82\xB1")
-
-color_template="\\[\\e%s\\]"
-
-USERNAME_FG=250
-USERNAME_BG=240
-USERNAME_ROOT_BG=124
-
-HOSTNAME_FG=250
-HOSTNAME_BG=238
-
 HOME_SPECIAL_DISPLAY=true
 HOME_BG=31  # blueish
 HOME_FG=15  # white
-PATH_BG=237  # dark grey
-PATH_FG=250  # light grey
 CWD_FG=254  # nearly-white grey
-SEPARATOR_FG=244
 
 READONLY_BG=124
 READONLY_FG=254
@@ -34,63 +19,148 @@ REPO_DIRTY_FG=15  # white
 JOBS_FG=39                                                                                                                              
 JOBS_BG=238                                                                                                                             
 
-CMD_PASSED_BG=236                                                                                                                       
-CMD_PASSED_FG=15                                                                                                                        
-CMD_FAILED_BG=161                                                                                                                       
-CMD_FAILED_FG=15
-
 SVN_CHANGES_BG=148                                                                                                                      
 SVN_CHANGES_FG=22  # dark green    
 
+#################################################################
+# Initial Environment
+#################################################################
+
+#################################################################
+# Symbols
+#################################################################
+## lock, network, separator, separator_thin
+symbol_lock=$(printf "%b" "\xEE\x82\xA2")
+symbol_network=$(printf "%b" "\xEE\x82\xA2")
+symbol_separator=$(printf "%b" "\xEE\x82\xB0")
+symbol_separator_thin=$(printf "%b" "\xEE\x82\xB1")
+## ahead and behind
+symbol_ahead=$(printf "%b" "\xE2\x87\xA1")
+symbol_behind=$(printf "%b" "\xE2\x87\xA3")
+
+#################################################################
+# Color
+#################################################################
+git_fg_clean=0
+git_fg_dirty=15
+git_bg_clean=148
+git_bg_dirty=161
+
+username_fg=250
+username_bg=240
+username_root_bg=124
+
+hostname_fg=101
+hostname_bg=47
+
+path_bg=237  # dark grey
+path_fg=250  # light grey
+
+separator_fg=244
+
+cmd_passed_bg=236
+cmd_passed_fg=15
+cmd_failed_bg=161
+cmd_failed_fg=15
+
+#################################################################
+# Function
+#################################################################
+## format
 function color()
 {
 	prefix=$1
 	code=$2
-	result=$(printf "[%s;5;%sm" $prefix $code)
-	echo $(printf "%b" $result)
+	echo $(printf "[%s;5;%sm" $prefix $code)
 	return 0
 }
-
 function fgcolor()
 {
 	code=$1
 	echo $(color "38" $code)
 	return 0
 }
-
 function bgcolor()
 {
 	code=$1
 	echo $(color "48" $code)
 	return 0
 }
+function format_color()
+{
+	echo "\[\\e$(fgcolor $1)\]\[\\e$(bgcolor $2)\]"
+}
+function format()
+{
+	echo $(format_color $1 $2)$3
+}
 
-echo $(fgcolor 250)
-
-segments=()
-segments[0]=2
-
-echo ${segments[0]}
-
+## git
 GIT=false
-SVN=false
-
+git_ps1=""
+git_fg=$git_fg_clean
+git_bg=$git_bg_clean
 function get_git_status()
 {
-	GIT_STATUS=$(git status --ignore-submodules)
+	git_status=$(git status --ignore-submodules)
 	GIT=$([ $? -eq 0 ] && echo true)
-	has_pending_commits=true
-	has_untracked_files=false
-	origin_position=""
+	if [[ "$GIT" = true ]]; then
+		SED="sed -r"
+		if [[ "$(uname -s)" = "Darwin" ]]; then
+			SED="sed -E"
+		fi
+		#git_status="On branch xxoo Your branch is ahead of 'origin/master' by 1 commit and have 5 and 4 different commits each. (use \"git push\" to publish your local commits) Changes not staged for commit: (use \"git add <file>...\" to update what will be committed) (use \"git checkout -- <file>...\" to discard changes in working directory) modified: powerline.sh no changes added to commit (use \"git add\" and/or \"git commit -a\")"
+		
+		branch=$(echo $git_status | $SED "s/^On branch ([a-z]+) .*/\1/g")
+		has_pending_commits=true
+		has_untracked_files=false
+		origin_position=""
 
-	if [[ "$GIT_STATUS" =~ "nothing to commit" ]]; then
-		has_pending_commits=false
-	fi
+		if [[ "$git_status" =~ "nothing to commit" ]]; then
+			has_pending_commits=false
+		fi
 
-	if [[ "$GIT_STATUS" =~ "Untracked files" ]]; then
-		has_untracked_files=true
+		if [[ "$git_status" =~ "Untracked files" ]]; then
+			has_untracked_files=true
+		fi
+
+		if [[ "$git_status" =~ "Your branch is ahead" ]] || [[ "$git_status" =~ "Your branch is behind" ]]; then
+			origin_status=$(echo $git_status | $SED "s/.*Your branch is (ahead|behind) of .* by ([0-9]+) commit.*/\1 \2/g")
+			if [[ "$origin_status" =~ "ahead" ]]; then
+				origin_position="$(echo $origin_status | $SED 's/[a-z]+ //g')$symbol_ahead"
+			else
+				origin_position="$(echo $origin_status | $SED 's/[a-z]+ //g')$symbol_behind"
+			fi
+		fi
+		diverged_pattern="and have [0-9]+ and [0-9]+ different commits each"
+		if [[ "$git_status" =~ $diverged_pattern ]]; then
+			diverged_status=$(echo $git_status | $SED "s/.*and have ([0-9]+) and ([0-9]+) different commits each.*/\1 \2/g")
+			origin_position="$(echo $diverged_status | cut -d ' ' -f 1)$symbol_ahead$(echo $diverged_status | cut -d ' ' -f 2)$symbol_behind"
+		fi
+
+		result=" $branch $origin_position"
+		if [[ "$has_untracked_files" = true ]]; then
+			result="$result+"
+		fi
+		if [[ "$has_pending_commits" = true ]] || [[ "$has_untracked_files" = true ]]; then
+			git_fg=$git_fg_dirty
+			git_bg=$git_bg_dirty
+		fi
+		git_ps1=$result
 	fi
 }
 
+ps1="$(format $username_fg $username_bg ' \u') $(format $username_bg $hostname_bg $symbol_separator)$(format $hostname_fg $hostname_bg ' \h')"
+ps1="$ps1 $(format $hostname_bg $path_bg $symbol_separator)$(format $path_fg $path_bg ' \W')"
 get_git_status
-echo $GIT $has_untracked_files
+if [[ "$GIT" = true ]]; then
+	ps1="$ps1 $(format $path_bg $git_bg $symbol_separator)$(format $git_fg $git_bg "$git_ps1") $(format $git_bg $cmd_passed_bg $symbol_separator)"
+else
+	ps1="$ps1 $(format $path_bg $cmd_passed_bg $symbol_separator)"
+fi
+
+ps1="$ps1$(format $cmd_passed_fg $cmd_passed_bg ' \$') \[\\e[0m\]\[\\e$(fgcolor $cmd_passed_fg)\]$symbol_separator \[\\e[0m\]"
+
+echo $ps1
+
+SVN=false
